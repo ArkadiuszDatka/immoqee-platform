2ce9199b3237422cff255bf88cd64e077e22ca37
Arkadiusz Datka <datkaarkadiusz5@gmail.com>
2021-01-09 15:59:00 +0100

    home page

--- immoqee-platform/src/components/list_of_forms.js
+++ immoqee-platform/src/components/list_of_forms.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./list_of_forms.css";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import { colors } from "@material-ui/core";
 
 const ListOfForms = (props) => {
   const [data, setData] = useState(props.list);
@@ -13,15 +17,28 @@ const ListOfForms = (props) => {
     <div className="listOfForms">
       {data.map((item, i) => {
         return (
-          <button
-            className="listOfFormsCard"
-            onClick={() => {
-              props.editForm(data[i]);
-              props.editState(true);
-            }}
-          >
-            {item.name}
-          </button>
+          <div className="listOfFormsCard">
+            <div class="rowElements">
+              <h6>{item.name}</h6>
+              <IconButton
+                id="btn"
+                style={{ color: colors.grey }}
+                aria-label="Uzupełnij"
+                onClick={(e) => {
+                  props.editForm(data[i]);
+                  props.editState(true);
+                }}
+              ><AddIcon /></IconButton>
+              <IconButton
+                id="btn"
+                style={{ color: colors.grey }}
+                aria-label="Cofnij"
+                onClick={(e) => {
+                  setData(data.splice(i, 1));
+                }}
+              ><DeleteIcon /></IconButton>
+            </div>
+          </div>
         );
       })}
     </div>
